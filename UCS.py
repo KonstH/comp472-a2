f3b226194a682c97aa6124037f1281799e4e4eb6
KonstH <konstantinhristev@gmail.com>
2020-11-10 17:36:30 -0500

    Adds timeout argument to the ucs function, handles special case of solution taking too long to be found

--- UCS.py
+++ UCS.py
@@ -1,26 +1,35 @@
 from PriorityQueue import PQ
 import numpy as np
+from time import perf_counter
 
-def ucs(rootNode):
+def ucs(rootNode, timeout):
   open_list = PQ()
   closed_list = set()   # set makes sure there's no duplicates
   open_list.insert(0, rootNode)  # Add initial node and total cost of 0
+  start_time = perf_counter()
+  timedOut = False
 
   while(open_list.notEmpty()):
-    totalCost, node = open_list.pop()
+    elapsed_time = perf_counter()
+    if ((elapsed_time - start_time) >= timeout):
+      timedOut = True
+      return (None, None, None, None, None, timedOut)
+    else:
+      totalCost, node = open_list.pop()
 
-    # if node has not been visited yet, go for it
-    if node not in closed_list:
-      closed_list.add(node)   # node is visited for the first time, add to closed list
-      if (node.isGoal(node.state)):
-        moves, costs, path = node.traceSolution([],[],[])
-        return (moves, costs, path, totalCost)   # Returns final solution
-      else:
-        node.generateSuccessors()
-        successors = node.successors
+      # if node has not been visited yet, go for it
+      if node not in closed_list:
+        closed_list.add(node)   # node is visited for the first time, add to closed list
+        if (node.isGoal(node.state)):
+          moves, costs, path = node.traceSolution([],[],[])
+          end_time = (elapsed_time - start_time)
+          return (moves, costs, path, totalCost, end_time, timedOut)   # Returns final solution
+        else:
+          node.generateSuccessors()
+          successors = node.successors
 
-        for successor in successors:
-          if successor not in closed_list:
-            new_cost = totalCost + successor.cost
-            open_list.insert(new_cost, successor)
+          for successor in successors:
+            if successor not in closed_list:
+              new_cost = totalCost + successor.cost
+              open_list.insert(new_cost, successor)
             
--- main.py
+++ main.py
@@ -31,25 +31,32 @@ def solve(puzzle, puzzleCount):
   print(np.matrix(puzzle))
   print('\nFinding solution with Uniform Cost Search....\n')
 
-  moves, costs, path, total_cost = ucs(rootNode)
-
-  moves.reverse()    # Show solution moves in correct order
-  costs.reverse()    # Show solution moves in correct order
-  path.reverse()    # Show solution moves in correct order
+  moves, costs, path, total_cost, end_time, timedOut = ucs(rootNode, 60)
 
   file_name = str(puzzleCount) + '_ucs_solution.txt'
   f = open(file_name,'w')
 
-  i = 0 
-  for state in path:
-    f.write(str(moves[i]) + ' ')  # Write the tile moved
-    f.write(str(costs[i]) + ' ')  # Write how much the move cost
-    f.write((' '.join(str(tile) for tile in state)).replace('[', '').replace(']', '').replace(', ', ' '))   # Write the state of the board after the move
-    f.write('\n')
-    i += 1
-  f.write(str(total_cost))
-  
-  print("Solution exported to " + file_name + "!\n")
+  if(timedOut):
+    print('Timed Out!\n')
+    f.write('no solution')
+    return
+  else:
+    moves.reverse()    # Show solution moves in correct order
+    costs.reverse()    # Show solution moves in correct order
+    path.reverse()    # Show solution moves in correct order
+    i = 0 
+
+    for state in path:
+      f.write(str(moves[i]) + ' ')  # Write the tile moved
+      f.write(str(costs[i]) + ' ')  # Write how much the move cost
+      f.write((' '.join(str(tile) for tile in state)).replace('[', '').replace(']', '').replace(', ', ' '))   # Write the state of the board after the move
+      f.write('\n')
+      i += 1
+
+    f.write(str(total_cost) + ' ')  # Write total cost
+    f.write(str(round(end_time, 1)))  # Write total computation time
+    
+    print("Solution exported to " + file_name + "!\n")
   
 
 # Solves each puzzle and outputs results/metrics in text file
